d181e56f673a52c2889de4760a633c73e29a7768
oreste-abizera <oresteabizera11@gmail.com>
2020-12-08 07:41:55 +0200

    Presentational and Container Components

---
+++ src/components/MenuComponent.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import {
+  Card,
+  CardImg,
+  CardImgOverlay,
+  CardText,
+  CardBody,
+  CardTitle,
+} from "reactstrap";
+
+export default class Menu extends Component {
+  render() {
+    const menu = this.props.dishes.map((dish) => {
+      return (
+        <div className="col-12 col-md-5 m-1">
+          <Card key={dish.id} onClick={() => this.props.onClick(dish.id)}>
+            <CardImg width="100%" src={dish.image} alt={dish.name} />
+            <CardImgOverlay>
+              <CardTitle>{dish.name}</CardTitle>
+            </CardImgOverlay>
+          </Card>
+        </div>
+      );
+    });
+
+    return (
+      <div className="container">
+        <div className="row">{menu}</div>
+      </div>
+    );
+  }
+}
