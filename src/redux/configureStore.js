57e4b692d7cc2500a9500c14016ae4e4271e8f02
oreste-abizera <oresteabizera11@gmail.com>
2020-12-27 12:26:05 +0200

    Combining Reducers

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,10 +1,17 @@
-import { createStore } from "redux";
-import { initialState, Reducer } from "./reducer";
+import { combineReducers, createStore } from "redux";
+import { Comments } from "./comments";
+import { Dishes } from "./dishes";
+import { Leaders } from "./leaders";
+import { Promotions } from "./promotions";
 
 export const ConfigureStore = () => {
   const store = createStore(
-    Reducer, //reducer
-    initialState //our initial store
+    combineReducers({
+      dishes: Dishes,
+      comments: Comments,
+      promotions: Promotions,
+      leaders: Leaders,
+    })
   );
   return store;
 };
